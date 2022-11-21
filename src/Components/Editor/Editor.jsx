50292677bbbae0281b3d9767a0d1a4b58332bafb
NikhilRohan <nikhilrohan0108@gmail.com>
2022-11-21 20:36:21 +0530

    Editor Component Added

--- src/Components/Editor/Editor.jsx
+++ src/Components/Editor/Editor.jsx
@@ -1,7 +1,36 @@
 import "/src/Components/Editor/Editor.css";
+import * as React from "react";
+import ReactMde from "react-mde";
+import * as Showdown from "showdown";
+import "react-mde/lib/styles/css/react-mde-all.css";
+
+const converter = new Showdown.Converter({
+  tables: true,
+  simplifiedAutoLink: true,
+  strikethrough: true,
+  tasklists: true,
+});
 
 const Editor = () => {
-  return <section className="editor"></section>;
+  const [value, setValue] = React.useState(
+    "# Type your markdown note's title here"
+  );
+  const [selectedTab, setSelectedTab] = React.useState("preview");
+  return (
+    <div className="container">
+      <ReactMde
+        value={value}
+        onChange={setValue}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={(markdown) =>
+          Promise.resolve(converter.makeHtml(markdown))
+        }
+        minEditorHeight={80}
+        heightUnits="vh"
+      />
+    </div>
+  );
 };
 
 export default Editor;
